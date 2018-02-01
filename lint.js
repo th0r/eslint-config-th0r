ef20613a1cf279d91111e00147348c8fcff380ee
th0r <grunin.ya@ya.ru>
2018-02-01 15:18:49 +0300

    Fix bug in lint.js

--- lint.js
+++ lint.js
@@ -14,9 +14,9 @@ const args = process.argv.slice(2);
 const fix = args.includes('-f');
 
 try {
-  const cmd = `./node_modules/.bin/eslint -c ${fix ? '--fix' : ''} ./index.json ${SOURCES}`;
+  const cmd = `./node_modules/.bin/eslint -c ./index.json ${fix ? '--fix' : ''} -- ${SOURCES}`;
   console.log(`Running the following command:\n${cmd}`);
-  execSync(cmd);
+  execSync(cmd, {stdio: 'inherit'});
   console.log('OK');
 } catch (err) {
   process.exit(err.status);
